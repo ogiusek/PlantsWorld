b59b4a0e0ce396ec8935a138ef4bde6f0ffbcb61
ogius <kowalewski.olgierd@gmail.com>
2022-12-05 14:32:22 +0100

    Repaired init players code problem

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from "./App.module.css";
 
 import player from './additionalFiles/initPlayer';
 import defaultAi from './additionalFiles/defaultAi';
 
 import NextGen from './NextGen/NextGen';
-// import Canvas from './Canvas/Canvas';
 
 function App() {
   const [players, setPlayers] = useState([player]);
   const [codes, setCodes] = useState([{ name: defaultAi.name, ai: Array.from(defaultAi.ai) }]);
   const [money, setMoney] = useState(0);
-  // const [simulates, setSimulates] = useState(false);
+
+  useEffect(() => {
+    let array = Array.from(players);
+    array[0].code = codes[0];
+    setPlayers(array);
+  }, []);
 
   const get = {
     players: players,
@@ -22,7 +26,6 @@ function App() {
   const set = {
     money: setMoney,
     players: setPlayers,
-    // simulates: setSimulates,
     codes: setCodes
   }
 
